64a1b72aa2536f6897c233cf9cdd4c959bc2e996
Andrew M. Webb <amwebb@acm.org>
2023-05-01 18:08:06 -0500

    fix camera flip issue.

--- s2p/p5rg/camera.js
+++ s2p/p5rg/camera.js
@@ -1,9 +1,7 @@
 // Set constraints for the video stream
 var constraints = { video: { facingMode: "environment" }, audio: false };// Define constants
-const cameraView = document.querySelector("#camera--view"),
-    cameraOutput = document.querySelector("#camera--output"),
-    cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")// Access the device camera and stream to cameraView
+const cameraView = document.querySelector("#camera--view");
+cameraView.controls = false;
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
