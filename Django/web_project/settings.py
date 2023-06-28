6340aa4705683b47a718eb06dc1eb9cb4965fe24
Andrew M. Webb <amwebb@acm.org>
2023-06-28 12:33:23 -0500

    update settings file to use environment variables; add additional requirements for cloud storage.

--- Django/web_project/settings.py
+++ Django/web_project/settings.py
@@ -10,28 +10,80 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
-from pathlib import Path
+import io
 import os
+from urllib.parse import urlparse
+
+import environ
+import google.auth
+from google.cloud import secretmanager
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+# [START cloudrun_django_secret_config]
+# SECURITY WARNING: don't run with debug turned on in production!
+# Change this to "False" when you are ready for production
+env = environ.Env(DEBUG=(bool, True))
+env_file = os.path.join(BASE_DIR, ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^71k_pf3-l&2%h!(hjn$ajj3#s74d+u!=xc+w@y8%qk_!xq9zg'
+try:
+    _, os.environ["GOOGLE_CLOUD_PROJECT"] = google.auth.default()
+except google.auth.exceptions.DefaultCredentialsError:
+    pass
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if os.path.isfile(env_file):
+    # Use a local secret file, if provided
+
+    env.read_env(env_file)
+# [START_EXCLUDE]
+elif os.getenv("TRAMPOLINE_CI", None):
+    # Create local settings if running with CI, for unit testing
+
+    placeholder = (
+        f"SECRET_KEY=a\n"
+        "GS_BUCKET_NAME=None\n"
+        f"DATABASE_URL=sqlite://{os.path.join(BASE_DIR, 'db.sqlite3')}"
+    )
+    env.read_env(io.StringIO(placeholder))
+# [END_EXCLUDE]
+elif os.environ.get("GOOGLE_CLOUD_PROJECT", None):
+    # Pull secrets from Secret Manager
+    project_id = os.environ.get("GOOGLE_CLOUD_PROJECT")
+
+    client = secretmanager.SecretManagerServiceClient()
+    settings_name = os.environ.get("SETTINGS_NAME", "django_settings")
+    name = f"projects/{project_id}/secrets/{settings_name}/versions/latest"
+    payload = client.access_secret_version(name=name).payload.data.decode("UTF-8")
+
+    env.read_env(io.StringIO(payload))
+else:
+    raise Exception("No local .env or GOOGLE_CLOUD_PROJECT detected. No secrets found.")
+# [END cloudrun_django_secret_config]
+SECRET_KEY = env("SECRET_KEY")
+
+DEBUG = env("DEBUG")
 
 # propoerties for selecting which DB to use
-DB = "local" #"postgres"
-PROXY = True
+# DB = "local" #"postgres"
+# PROXY = True
 
-ALLOWED_HOSTS = ['*']
+# [START cloudrun_django_csrf]
+# SECURITY WARNING: It's recommended that you use this when
+# running in production. The URL will be known once you first deploy
+# to Cloud Run. This code takes the URL and converts it to both these settings formats.
+CLOUDRUN_SERVICE_URL = env("CLOUDRUN_SERVICE_URL", default=None)
+if CLOUDRUN_SERVICE_URL:
+    ALLOWED_HOSTS = [urlparse(CLOUDRUN_SERVICE_URL).netloc]
+    CSRF_TRUSTED_ORIGINS = [CLOUDRUN_SERVICE_URL]
+    SECURE_SSL_REDIRECT = True
+    SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
+else:
+    ALLOWED_HOSTS = ["*"]
+# [END cloudrun_django_csrf]
 
 
 # Application definition
@@ -80,37 +132,44 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-if (DB == "postgres"):
-    if (PROXY):
-        DB_HOST = "127.0.0.1"
-        OPTIONS = {}
-    else:
-        DB_HOST = "/cloudsql/livingsound:us-central1:livingsound-db"
-        OPTIONS = {
-            "sslmode": 'verify-ca',
-            "sslrootcert": os.path.join(BASE_DIR, 'server-ca.pem'),
-            "sslcert": os.path.join(BASE_DIR, 'client-cert.pem'),
-            "sslkey": os.path.join(BASE_DIR, 'client-key.pem'),
-        }
+DATABASES = {"default": env.db()}
 
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "postgres",
-            "USER": "django",
-            "PASSWORD": "django",
-            "HOST": DB_HOST,
-            "PORT": "5432",
-            "OPTIONS": OPTIONS
-        }
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+# If the flag as been set, configure to use proxy
+if os.getenv("USE_CLOUD_SQL_AUTH_PROXY", None):
+    DATABASES["default"]["HOST"] = "127.0.0.1"
+    DATABASES["default"]["PORT"] = 5432
+
+# if (DB == "postgres"):
+#     if (PROXY):
+#         DB_HOST = "127.0.0.1"
+#         OPTIONS = {}
+#     else:
+#         DB_HOST = "/cloudsql/livingsound:us-central1:livingsound-db"
+#         OPTIONS = {
+#             "sslmode": 'verify-ca',
+#             "sslrootcert": os.path.join(BASE_DIR, 'server-ca.pem'),
+#             "sslcert": os.path.join(BASE_DIR, 'client-cert.pem'),
+#             "sslkey": os.path.join(BASE_DIR, 'client-key.pem'),
+#         }
+
+#     DATABASES = {
+#         "default": {
+#             "ENGINE": "django.db.backends.postgresql",
+#             "NAME": "postgres",
+#             "USER": "django",
+#             "PASSWORD": "django",
+#             "HOST": DB_HOST,
+#             "PORT": "5432",
+#             "OPTIONS": OPTIONS
+#         }
+#     }
+# else:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
 
 
 # Password validation
@@ -158,3 +217,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap4'
 
 #CRISPY_TEMPLATE_PACK = 'bootstrap4'
+# Static files (CSS, JavaScript, Images)
+# [START cloudrun_django_static_config]
+# Define static storage via django-storages[google]
+GS_BUCKET_NAME = env("GS_BUCKET_NAME")
+DEFAULT_FILE_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
+STATICFILES_STORAGE = "storages.backends.gcloud.GoogleCloudStorage"
+GS_DEFAULT_ACL = "publicRead"
+# [END cloudrun_django_static_config]
+
+MEDIA_URL = f"https://storage.googleapis.com/{GS_BUCKET_NAME}/"
+     
