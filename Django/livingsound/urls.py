9110caf6091dbf40460efc762ec45098b31683a9
Andrew M. Webb <amwebb@acm.org>
2023-06-28 18:15:41 -0500

    create login page template and set login redirect to home page.

--- Django/livingsound/urls.py
+++ Django/livingsound/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 
@@ -6,4 +6,5 @@ urlpatterns = [
     path("", views.garden, name="garden"),
     path("submission/", views.submission, name="submission"),  
     path("success", views.success, name="success")
+    path("accounts/", include("django.contrib.auth.urls"))  
 ]
