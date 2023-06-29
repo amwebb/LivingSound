693c59175bbddba02655e1e01aa1f629af95c90e
Andrew M. Webb <amwebb@acm.org>
2023-06-29 09:43:20 -0500

    better generation of garden view

--- Django/livingsound/views.py
+++ Django/livingsound/views.py
@@ -30,81 +30,90 @@ def garden(request):
     currentUser = request.user.username
     """
 
-    #admin account has username id 1
-    try:
-        # Get the latest entry for the username 'pq'
-        p1Ent = GardenEntry.objects.filter(username=2).latest('timestamp')
-    except ObjectDoesNotExist:
-        p1Ent = None
+    # #admin account has username id 1
+    # try:
+    #     # Get the latest entry for the username 'pq'
+    #     p1Ent = GardenEntry.objects.filter(username=2).latest('timestamp')
+    # except ObjectDoesNotExist:
+    #     p1Ent = None
 
-    try:
-        p2Ent = GardenEntry.objects.filter(username=3).latest('timestamp')
-    except ObjectDoesNotExist:
-        p2Ent = None
+    # try:
+    #     p2Ent = GardenEntry.objects.filter(username=3).latest('timestamp')
+    # except ObjectDoesNotExist:
+    #     p2Ent = None
 
-    try:
-        p3Ent = GardenEntry.objects.filter(username=4).latest('timestamp')
-    except ObjectDoesNotExist:
-        p3Ent = None
+    # try:
+    #     p3Ent = GardenEntry.objects.filter(username=4).latest('timestamp')
+    # except ObjectDoesNotExist:
+    #     p3Ent = None
 
-    try:
-        p4Ent = GardenEntry.objects.filter(username=5).latest('timestamp')
-    except ObjectDoesNotExist:
-        p4Ent = None
+    # try:
+    #     p4Ent = GardenEntry.objects.filter(username=5).latest('timestamp')
+    # except ObjectDoesNotExist:
+    #     p4Ent = None
 
-    try:
-        p5Ent = GardenEntry.objects.filter(username=6).latest('timestamp')
-    except ObjectDoesNotExist:
-        p5Ent = None
+    # try:
+    #     p5Ent = GardenEntry.objects.filter(username=6).latest('timestamp')
+    # except ObjectDoesNotExist:
+    #     p5Ent = None
 
-    try:
-        p6Ent = GardenEntry.objects.filter(username=7).latest('timestamp')
-    except ObjectDoesNotExist:
-        p6Ent = None
+    # try:
+    #     p6Ent = GardenEntry.objects.filter(username=7).latest('timestamp')
+    # except ObjectDoesNotExist:
+    #     p6Ent = None
 
-    try:
-        p7Ent = GardenEntry.objects.filter(username=8).latest('timestamp')
-    except ObjectDoesNotExist:
-        p7Ent = None
+    # try:
+    #     p7Ent = GardenEntry.objects.filter(username=8).latest('timestamp')
+    # except ObjectDoesNotExist:
+    #     p7Ent = None
 
-    try:
-        p8Ent = GardenEntry.objects.filter(username=9).latest('timestamp')
-    except ObjectDoesNotExist:
-        p8Ent = None
+    # try:
+    #     p8Ent = GardenEntry.objects.filter(username=9).latest('timestamp')
+    # except ObjectDoesNotExist:
+    #     p8Ent = None
 
-    try:
-        p9Ent = GardenEntry.objects.filter(username=10).latest('timestamp')
-    except ObjectDoesNotExist:
-        p9Ent = None
+    # try:
+    #     p9Ent = GardenEntry.objects.filter(username=10).latest('timestamp')
+    # except ObjectDoesNotExist:
+    #     p9Ent = None
 
-    try:
-        p10Ent = GardenEntry.objects.filter(username=11).latest('timestamp')
-    except ObjectDoesNotExist:
-        p10Ent = None
+    # try:
+    #     p10Ent = GardenEntry.objects.filter(username=11).latest('timestamp')
+    # except ObjectDoesNotExist:
+    #     p10Ent = None
 
-    try:
-        p11Ent = GardenEntry.objects.filter(username=12).latest('timestamp')
-    except ObjectDoesNotExist:
-        p11Ent = None
+    # try:
+    #     p11Ent = GardenEntry.objects.filter(username=12).latest('timestamp')
+    # except ObjectDoesNotExist:
+    #     p11Ent = None
 
-    try:
-        p12Ent = GardenEntry.objects.filter(username=13).latest('timestamp')
-    except ObjectDoesNotExist:
-        p12Ent = None
+    # try:
+    #     p12Ent = GardenEntry.objects.filter(username=13).latest('timestamp')
+    # except ObjectDoesNotExist:
+    #     p12Ent = None
+
+    entries = []
+    for i in range(2,13):
+        try:
+            entry = GardenEntry.objects.filter(username=i).latest('timestamp')
+        except ObjectDoesNotExist:
+            entry = None
+        entries.append(entry)
 
     context = {
-        'p1Ent': p1Ent,
-        'p2Ent': p2Ent,
-        'p3Ent': p3Ent,
-        'p4Ent': p4Ent,
-        'p5Ent': p5Ent,
-        'p6Ent': p6Ent,
-        'p7Ent': p7Ent,
-        'p8Ent': p8Ent,
-        'p9Ent': p9Ent,
-        'p10Ent': p10Ent,
-        'p11Ent': p11Ent,
-        'p12Ent': p12Ent,
+        "data" : entries,
+        # 'p1Ent': p1Ent,
+        # 'p2Ent': p2Ent,
+        # 'p3Ent': p3Ent,
+        # 'p4Ent': p4Ent,
+        # 'p5Ent': p5Ent,
+        # 'p6Ent': p6Ent,
+        # 'p7Ent': p7Ent,
+        # 'p8Ent': p8Ent,
+        # 'p9Ent': p9Ent,
+        # 'p10Ent': p10Ent,
+        # 'p11Ent': p11Ent,
+        # 'p12Ent': p12Ent,
         #'allEnts': allEnts,
         #'current_user': current_user
         }
