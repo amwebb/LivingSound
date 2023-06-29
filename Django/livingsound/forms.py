476fee0c734eb39095c60c3aba6cd734f6e16a69
Andrew Webb <andrewwebb@lsu.edu>
2023-06-29 18:40:51 -0500

    new emoji slider widget for entries.

--- Django/livingsound/forms.py
+++ Django/livingsound/forms.py
@@ -1,18 +1,23 @@
 from django import forms
 from .models import GardenEntry
 from django.forms import Textarea
+from django.forms.widgets import NumberInput
+
+class RangeInput(NumberInput):
+    template_name = 'widgets/slider.html'
+
 
 class GardenForm(forms.ModelForm):
 
-    choices=(
-        ("1", "1"),
-        ("2", "2"),
-        ("3", "3"),
-        ("4", "4"),
-        ("5", "5"),
-        )
+    # choices=(
+    #     ("1", "1"),
+    #     ("2", "2"),
+    #     ("3", "3"),
+    #     ("4", "4"),
+    #     ("5", "5"),
+    #     )
 
-    rating = forms.ChoiceField(choices=choices)
+    #rating = forms.ChoiceField(choices=choices)
 
     class Meta:
         model = GardenEntry
@@ -20,5 +25,6 @@ class GardenForm(forms.ModelForm):
         exclude = ("username", )
         widgets = {
           'message': Textarea(attrs={'rows':10, 'cols':45}),
+          'rating': RangeInput,
         }
         
--- Django/livingsound/views.py
+++ Django/livingsound/views.py
@@ -11,120 +11,24 @@ from .forms import GardenForm
 # Decorator for Login Required
 from django.contrib.auth.decorators import login_required
 
-MIN_USER_ID = 2
-NUM_USERS = 11
-
 @login_required(login_url='/accounts/login/')
 def garden(request):
-    """Filter each username. find the most recent entry by that username.
-    add it to the context list as 'username'Ent. use the values in each
-    username to fill in info in entry.html"""
-
-
-    #Testing for getting current user - can delete once website is done
-    """
-    allEnts = GardenEntry.objects.all().values()
-    if str(request.user.username) == 'p1':
-        current_user = 'true'
-    else:
-        current_user = 'false'
-
-    currentUser = request.user.username
-    """
-
-    # #admin account has username id 1
-    # try:
-    #     # Get the latest entry for the username 'pq'
-    #     p1Ent = GardenEntry.objects.filter(username=2).latest('timestamp')
-    # except ObjectDoesNotExist:
-    #     p1Ent = None
-
-    # try:
-    #     p2Ent = GardenEntry.objects.filter(username=3).latest('timestamp')
-    # except ObjectDoesNotExist:
-    #     p2Ent = None
-
-    # try:
-    #     p3Ent = GardenEntry.objects.filter(username=4).latest('timestamp')
-    # except ObjectDoesNotExist:
-    #     p3Ent = None
-
-    # try:
-    #     p4Ent = GardenEntry.objects.filter(username=5).latest('timestamp')
-    # except ObjectDoesNotExist:
-    #     p4Ent = None
-
-    # try:
-    #     p5Ent = GardenEntry.objects.filter(username=6).latest('timestamp')
-    # except ObjectDoesNotExist:
-    #     p5Ent = None
-
-    # try:
-    #     p6Ent = GardenEntry.objects.filter(username=7).latest('timestamp')
-    # except ObjectDoesNotExist:
-    #     p6Ent = None
-
-    # try:
-    #     p7Ent = GardenEntry.objects.filter(username=8).latest('timestamp')
-    # except ObjectDoesNotExist:
-    #     p7Ent = None
-
-    # try:
-    #     p8Ent = GardenEntry.objects.filter(username=9).latest('timestamp')
-    # except ObjectDoesNotExist:
-    #     p8Ent = None
-
-    # try:
-    #     p9Ent = GardenEntry.objects.filter(username=10).latest('timestamp')
-    # except ObjectDoesNotExist:
-    #     p9Ent = None
-
-    # try:
-    #     p10Ent = GardenEntry.objects.filter(username=11).latest('timestamp')
-    # except ObjectDoesNotExist:
-    #     p10Ent = None
-
-    # try:
-    #     p11Ent = GardenEntry.objects.filter(username=12).latest('timestamp')
-    # except ObjectDoesNotExist:
-    #     p11Ent = None
-
-    # try:
-    #     p12Ent = GardenEntry.objects.filter(username=13).latest('timestamp')
-    # except ObjectDoesNotExist:
-    #     p12Ent = None
-
-    participants_group = Group.objects.get(name="Participants")
     entries = []
-    non_admin_users = User.objects.filter(is_staff=False)
+    non_admin_users = User.objects.filter(groups__name='Participants').order_by('username')
 
     for user in non_admin_users:
         try:
             entry = GardenEntry.objects.filter(username=user).latest('timestamp')
             entries.append(entry)
         except ObjectDoesNotExist:
-            entry = None
+            pass
         
 
-    entries.reverse()
+    #entries.reverse()
 
     context = {
         "data" : entries,
-        # 'p1Ent': p1Ent,
-        # 'p2Ent': p2Ent,
-        # 'p3Ent': p3Ent,
-        # 'p4Ent': p4Ent,
-        # 'p5Ent': p5Ent,
-        # 'p6Ent': p6Ent,
-        # 'p7Ent': p7Ent,
-        # 'p8Ent': p8Ent,
-        # 'p9Ent': p9Ent,
-        # 'p10Ent': p10Ent,
-        # 'p11Ent': p11Ent,
-        # 'p12Ent': p12Ent,
-        #'allEnts': allEnts,
-        #'current_user': current_user
-        }
+    }
 
     return render(request, 'entry.html', context=context)
 
